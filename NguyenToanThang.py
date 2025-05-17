127cc49e05dea376a8e9d3471d2cb14edb9a28cc
Bo1410 <jen29112004@gmail.com>
2025-05-17 15:02:46 +0700

    kaka

---
+++ NguyenToanThang.py
@@ -0,0 +1,26 @@
+x = 30
+y = 21
+namek = "NguyenToanThang"
+print(x ," - ",y," = ",x-y)
+print(x ," + ",y," = ",x+y)
+print(x ," * ",y," = ",x*y)
+print(x ," / ",y," = ",x/y)
+print(x ," % ",y," = ",x%y)
+is_valid=True
+if is_valid:
+    {
+        print(namek," age: ",y)   
+    }
+print(x!=31)
+print(x!=30)
+name = input("Nhap ten cua ban: ")
+print("Xin chao, ",name)
+print(name[0])
+print(name[3:])
+fruits=["a","b","c"]
+for fr in fruits:
+    print("fr: ", fr)
+
+def abc(n):
+    print(n)
+print(abc(3))
